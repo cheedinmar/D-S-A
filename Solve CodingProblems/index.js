4bdf6f8e27f0d72dc405a042a40d12adca18c231
Blessing Onwunali <cheedinmarblessing@gmail.com>
2022-09-08 23:04:35 +0100

    section: solve coding problems

---
+++ Solve CodingProblems/index.js
@@ -0,0 +1,14 @@
+/**HOW TO SOLVE CODING PROBLEMS
+ * Analytical skills: how you can think through the problems and analyze things
+ * Coding skills: do you code well, by writing clean, simple organised and readable code?
+ * Technical knowledge: do you know the fundamentals of the job you are applying for?
+ * Communication skills: does your personality matches the companie's culture
+ * 
+ * In order to solve coding problems we need to learn these data structures and algorithm
+ * Array, Stacks, Queues, Linked List, Trees, Tries, Graphs, Hash Tables
+ * Sorting, Dynamic Programming, BFS + DFS (searching), Recursion
+ * 
+ * 
+ * STEP BY STEP THROUGH A PROBLEM IN A CODING INTERVIEW
+ * 
+ */
